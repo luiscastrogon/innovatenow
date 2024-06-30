577700efe6b131b8b4652b7cccbfa14154902fab
Luis Castro G <lu.castrog@duocuc.cl>
2024-06-30 18:26:36 -0400

    request bbdd , permisos a bbdd bryan

--- home/views.py
+++ home/views.py
@@ -11,7 +11,11 @@ def index(request):
 
 def index2(request):
 	navbarmenus = navbarmenu.objects.all()
-	context = { "navbarmenus": navbarmenus }	
+	staffs = staff.objects.all()
+	context = { 
+        "navbarmenus": navbarmenus,
+        "staffs": staffs    
+    }	
 	return render (request, 'quien/index2.html', context)
 
 def index3(request):
@@ -19,7 +23,3 @@ def index3(request):
 	context = { "navbarmenus": navbarmenus }	
 	return render (request, 'servicios/index3.html', context)
 	
-def staff1(request):
-    staffs = staff.objects.all()
-    context = { "staffs": staffs }
-    return render (request, 'quien/index2.html', context)
